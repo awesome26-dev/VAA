1a1b57c2bb8f64f678c7b2e34168dbcbf74f73cd
Nick Birgfeld <nickbirgfeld676@gmail.com>
2023-07-18 12:23:58 +0200

    - Deleting Files

--- src/main/java/de/vaa/awesome/VAAApplication.java
+++ src/main/java/de/vaa/awesome/VAAApplication.java
@@ -1,38 +1,14 @@
 package de.vaa.awesome;
 
-import jakarta.persistence.EntityManagerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
 
-import javax.sql.DataSource;
-
-@EnableJpaRepositories(basePackages = "de.vaa.awesome.repository")
 @SpringBootApplication
-@EntityScan("de.vaa.awesome.entity")
 public class VAAApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(VAAApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(VAAApplication.class, args);
-	}
-
-	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-		emf.setDataSource(dataSource);
-		return emf;
-	}
-
-	@Bean
-	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-		return new JpaTransactionManager(emf);
-
-	}
 }
 
